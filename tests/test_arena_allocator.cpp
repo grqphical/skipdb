f69ee99e18d210860cd2988c186c6d08f0275063
Nathan Jacobson <nathan@nathanjacobson.ca>
2026-07-30 13:58:12 -0700

    updated tests to use self contained arenas

--- tests/test_arena_allocator.cpp
+++ tests/test_arena_allocator.cpp
@@ -9,30 +9,39 @@ TEST(ArenaAllocatorTests, TestInit) {
   ASSERT_EQ(_arena_allocator.size, 1024);
 
   ASSERT_NE((size_t)_arena_allocator.head, 0);
+
+  arena_deinit();
 }
 
 TEST(ArenaAllocatorTests, TestAllocate) {
+  arena_init(1024);
   size_t offset = arena_allocate(64);
   ASSERT_EQ(offset, 0);
   size_t offset_2 = arena_allocate(128);
   ASSERT_EQ(offset_2, 64);
+  arena_deinit();
 }
 
 TEST(ArenaAllocatorTests, TestAllocateBeyondSize) {
-  size_t offset = arena_allocate(2048);
-  ASSERT_EQ(offset, 64 + 128);
+  arena_init(1024);
+  size_t offset = arena_allocate(2056);
+  ASSERT_EQ(offset, 0);
   ASSERT_EQ(_arena_allocator.size, 3072);
+  arena_deinit();
 }
 
 TEST(ArenaAllocatorTests, TestOffsetToAddress) {
+  arena_init(1024);
   size_t offset = arena_allocate(1);
   uint32_t *ptr = (uint32_t *)ARENA_ADDR(offset);
   ASSERT_EQ(ptr, (uint32_t *)(_arena_allocator.head + offset));
   *ptr = 57;
   ASSERT_EQ(*(uint32_t *)(_arena_allocator.head + offset), 57);
+  arena_deinit();
 }
 
 TEST(ArenaAllocatorTests, TestDeInit) {
+  arena_init(1024);
   arena_deinit();
   ASSERT_EQ(_arena_created, 0);
   ASSERT_EQ((size_t)_arena_allocator.head, 0);
