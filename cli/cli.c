3915efc687c130baca6a90e8453b55a48f11f1ba
Nathan Jacobson <nathan@nathanjacobson.ca>
2026-08-15 22:20:23 -0700

    created basic read-print loop

--- cli/cli.c
+++ cli/cli.c
@@ -1,6 +1,72 @@
+#include "skipdb.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+#define CLI_LINE_BUF_SIZE 1024
+char *read_line(void) {
+  int bufsize = CLI_LINE_BUF_SIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char) * bufsize);
+  int c;
+
+  if (!buffer) {
+    fprintf(stderr, "skipdb_cli: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  while (1) {
+    c = getchar();
+
+    // If we hit EOF, replace it with a null character and return.
+    if (c == EOF || c == '\n') {
+      buffer[position] = '\0';
+      return buffer;
+    } else {
+      buffer[position] = c;
+    }
+    position++;
+
+    if (position >= bufsize) {
+      bufsize += CLI_LINE_BUF_SIZE;
+      buffer = realloc(buffer, bufsize);
+      if (!buffer) {
+        fprintf(stderr, "skipdb_cli: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
+}
 
 int main(int argc, char **argv) {
-  printf("Hello, World!");
+  skipdb_instance *db;
+  if (argc == 1) {
+    db = skipdb_init("<memory>");
+  } else if (argc == 2) {
+    if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
+      printf("usage: skipdb_cli <filename>\n");
+      return 0;
+    }
+
+    printf("this feature has not been implemented yet\n");
+    return 0;
+  } else {
+    printf("usage: skipdb_cli <filename>\n");
+    return -1;
+  }
+
+  char *line;
+
+  printf("skipdb_cli - type ':exit' to quit\n");
+  do {
+    printf("skipdb> ");
+    line = read_line();
+
+    if (strcmp(line, ":exit") == 0)
+      break;
+
+  } while (true);
+
+  skipdb_destroy(db);
+
   return 0;
 }
